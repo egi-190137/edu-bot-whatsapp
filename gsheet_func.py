a7a9754c2b402b5e93d7689c4fd74d0b46052051
egi-190137 <egiputra450@gmail.com>
2022-07-04 09:24:38 +0700

    fix variable

--- gsheet_func.py
+++ gsheet_func.py
@@ -11,15 +11,14 @@ creds  = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", sc
 client = gspread.authorize(creds)
 
 contacts = client.open("chatbot_data").worksheet("contacts")
-# quotes = client.open("chatbot_data").worksheet("quotes")
-# messages = client.open("chatbot_data").worksheet("quotes")
+quotes = client.open("chatbot_data").worksheet("quotes")
 
 list_guru = {
     'nama': contacts.col_values(1),
     'nomor': [ f'+{no}' for no in contacts.col_values(2) ]
 }
 
-quotes = quotes.col_values(1)
+array_quotes = quotes.col_values(1)
 
 def getRandomQuote():
     idx = random.randint(0, len(quotes) - 1)
